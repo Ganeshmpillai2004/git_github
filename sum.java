71c66580944a747a7644c34def690eeffcbffbc1
Ganesh <ganeshmpillai2004@gmail.com>
2026-02-03 11:09:50 +0530

    added sum file

---
+++ sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class sum {
+
+    public static void main(String ar[]) {
+    Scanner s=new Scanner(System.in);
+    System.out.println("enter two number :");
+    int a=s.nextInt();
+    int b=s.nextInt();
+    int c=a+b;
+    System.out.println("result is:"+c);
+    }
+}
