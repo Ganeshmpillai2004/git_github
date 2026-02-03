6712ce9849ace305ee97916f378c175947ed3d7e
Ganesh <ganeshmpillai2004@gmail.com>
2026-02-03 11:17:03 +0530

    modified sum file

--- sum.java
+++ sum.java
@@ -3,7 +3,7 @@ public class sum {
 
     public static void main(String ar[]) {
     Scanner s=new Scanner(System.in);
-    System.out.println("enter two number :");
+    System.out.println("enter two number \n:");
     int a=s.nextInt();
     int b=s.nextInt();
     int c=a+b;
